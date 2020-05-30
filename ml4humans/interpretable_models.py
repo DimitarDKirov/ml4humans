a3e97a81fe2833e91ee4b38799b9e532232d41d9
Martin Boyanov <martin.boyanov@smule.com>
2020-05-30 08:36:09 +0300

    add skeleton for interpretable models

---
+++ ml4humans/interpretable_models.py
@@ -0,0 +1,11 @@
+from sklearn.linear_model import LinearRegression
+
+
+def weight_plot(model: LinearRegression):
+    pass
+
+def effect_plot(model: LinearRegression, ds):
+    pass
+
+def effect_plot_for_example(model, ds, example):
+    pass
